14f1869f53c48ade500195c6db59d390de174a98
khushiweb434 <prabhakarkhushi8@gmail.com>
2026-05-24 15:00:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function cutCake(){
-  document.getElementById("msg").innerHTML =
-  "🎉 Happy Birthday My Love ❤️  my everything  💕";
+function cutCake() {
+
+  // 💖 Romantic Birthday Message
+  document.getElementById("cakeMessage").innerHTML =
+  "🎉 HAPPY BIRTHDAY MY LOVE ❤️ <br><br> You are my forever happiness 💕"; Today I’m not just wishing you Happy Birthday…😌 I’m replaying all our little memories in my head like my favorite movie 🎬💖
+Every scene has you smiling in it.
+Happy Birthday to the best part of my story 🎂✨    
+    Many many happpppyyyyyyyy  birthdayyyyyyy myyyy loveeee  ❤️  my home 🫶 myyy peace 💖  my everything 🥰
+
+  // 🎆 Confetti Animation
+  confetti({
+    particleCount: 250,
+    spread: 120,
+    origin: { y: 0.6 }
+  });
+
 }
